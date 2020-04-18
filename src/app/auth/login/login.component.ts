0ae675ff65614cc6695dcb685170d2390ce6e243
Carrie <carriescott0@gmail.com>
2020-04-18 13:42:10 +0100

    initial commit

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
+import {FlexOrderDirective} from '@angular/flex-layout';
+import { FormBuilder, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group ({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
+
+}
